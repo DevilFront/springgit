2989fae0c1c7d762bee16b77967dac93fda25e43
DevilFront <jwj2994@naver.com>
2020-07-10 15:19:40 +0900

    초기버전

---
+++ src/test/java/com/fragrance/web/SpringgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fragrance.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
